2611e86072427f9a5d85519268827d457b59da26
BrianKennedyN <amkamjoro@gmail.com>
2020-11-08 16:32:45 +0300

    added quote properties

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,10 @@
-export class Quotes {
+export class Quote {
+    showDescription: boolean;
+    public upvote:number;
+    public downvote:number;
+constructor(public id:number, public quote:string, public author:string, public name,public completeDate: Date){
+    this.showDescription=false;
+    this.upvote=0;
+    this.downvote=0;
+}
 }
